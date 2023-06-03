c5d8e0630b613fb5af12e0e91c39d090824a5bd9
Marius <marius.pocev@gmail.com>
2023-06-03 23:06:12 +0100

    Add code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import 'source-map-support/register'
+
+type MapFnParams<V> = Parameters<Parameters<Array<V>['map']>[0]>
+type MapFn<V, R> = (...params: MapFnParams<V>) => R
+type MapSiftFn<V, R> = (...params: MapFnParams<V>) => [] | [NonNullable<R>]
+
+const mapSift = <V, R>(mapFn: MapFn<V, R>): MapSiftFn<V, R> => (...params) => {
+    const result = mapFn(...params)
+    return result ? [result] : []
+}
+
+export default mapSift
